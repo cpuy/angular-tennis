563c82ca4f78249ec4dd1e6e927c8a6c19e351de
Colin PUY <colin.puy@bonitasoft.com>
2014-06-25 09:47:39 +0200

    project scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTennisApp
+ * @description
+ * # angularTennisApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTennisApp', []);
